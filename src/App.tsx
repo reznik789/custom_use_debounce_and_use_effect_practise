57b601011a1d153b6be0cf41e1d14c952d3264f1
CodeSandbox <support@codesandbox.io>
2023-12-19 15:20:08 +0000

    Sandbox snapshot commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,45 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
+import { useLayoutEffect, useMemo, useRef, useState, useEffect } from "react";
+import debounce from "lodash/fp/debounce";
 import "./App.css";
 
+type AnyFunction = (...args: any[]) => any;
+
+function useDebounce<TFunc extends AnyFunction>(
+  callback: TFunc,
+  timeout = 500,
+) {
+  const callbackRef = useRef(callback);
+  useLayoutEffect(() => {
+    callbackRef.current = callback;
+  }, [callback]);
+
+  return useMemo(
+    () =>
+      debounce(timeout, (...args: Parameters<TFunc>): ReturnType<TFunc> => {
+        return callbackRef.current(...args);
+      }),
+    [timeout],
+  );
+}
+
 function App() {
-  const [count, setCount] = useState(0);
+  const [state, setState] = useState(0);
+
+  const onChange = () => {
+    setState((prev) => prev + 1);
+  };
+
+  const sendData = useDebounce((data) => {
+    console.log(data);
+  }, 3000);
+
+  useEffect(() => {
+    sendData(state);
+  }, [state, sendData]);
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://reactjs.org" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-      </div>
-      <h1>React + Vite</h1>
-      <h2>On CodeSandbox!</h2>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR.
-        </p>
-
-        <p>
-          Tip: you can use the inspector button next to address bar to click on
-          components in the preview and open the code in the editor!
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div>
+      <button onClick={onChange}>Click me</button>
     </div>
   );
 }
